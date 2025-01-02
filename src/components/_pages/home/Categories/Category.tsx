5ecc76e052ed672d6798a27ce0a108ff18423f9e
marzieh <marziehmonshizade@yahoo.com>
2025-01-03 02:10:05 +0330

    Add: add category components

---
+++ src/components/_pages/home/Categories/Category.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+function Category({}: Props) {
+  return (
+    <div className="w-[75px] h-[99.99px]">
+      <div className="w-[52px] h-[52px] rounded-full bg-red-500"></div>
+      <span>Category</span>
+    </div>
+  );
+}
+
+export default Category;
